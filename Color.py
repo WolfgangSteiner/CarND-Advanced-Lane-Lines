f70ba61234733e4d3c008711750c4508d68425eb
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-01-30 09:38:05 +0100

    add more colors

--- Color.py
+++ Color.py
@@ -2,6 +2,8 @@ class color:
     green = (0,255,0)
     light_green = (127,255,127)
     red = (0,0,255)
+    orange = (0,165,255)
     pink = (186,0,255)
     blue = (255,0,0)
+    light_blue = (255,127,127)
     white = (255,255,255)
